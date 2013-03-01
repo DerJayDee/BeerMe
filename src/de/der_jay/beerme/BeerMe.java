28de2a5c39914a33b7fc2a37e57260ffb12ee6f9
Jan Degenhardt <jandegenhardt@web.de>
2013-03-01 16:10:33 +0100

    all buttons integrated and operating fully functional.

--- src/de/der_jay/beerme/BeerMe.java
+++ src/de/der_jay/beerme/BeerMe.java
@@ -16,10 +16,16 @@ import android.widget.Toast;
  */
 public class BeerMe extends Activity {
 
+	/** String for accessing the SharedPreferences set for SMALL BEER */
 	private final String SMALL_BEER = "de.der_jay.beerme.SMALL_BEER";
+
+	/** String for accessing the SharedPreferences set for LARGE BEER */
 	private final String LARGE_BEER = "de.der_jay.beerme.LARGE_BEER";
 
+	/** Counter for the amount of smallBeer */
 	private int smallBeer;
+
+	/** Counter for the amount of largeBeer */
 	private int largeBeer;
 
 	/**
@@ -41,16 +47,16 @@ public class BeerMe extends Activity {
 			smallBeer = savedInstanceState.getInt(SMALL_BEER);
 			largeBeer = savedInstanceState.getInt(LARGE_BEER);
 		}
-		
-		TextView tw = (TextView)findViewById(R.id.small_beer_count);
-		tw.setText(""+smallBeer);
-		tw = (TextView)findViewById(R.id.large_beer_count);
-		tw.setText(""+largeBeer);
+
+		TextView tw = (TextView) findViewById(R.id.small_beer_count);
+		tw.setText("" + smallBeer);
+		tw = (TextView) findViewById(R.id.large_beer_count);
+		tw.setText("" + largeBeer);
 	}
 
 	/**
-	 * This Method is called whenever the Activity is paused, exited
-	 * or destroyed. It saves the current set variables in the SharedPreferences.
+	 * This Method is called whenever the Activity is paused, exited or
+	 * destroyed. It saves the current set variables in the SharedPreferences.
 	 */
 	@Override
 	protected void onPause() {
@@ -83,8 +89,8 @@ public class BeerMe extends Activity {
 	 */
 	public void addSmallBeer(View view) {
 		smallBeer++;
-		postToast("small Beer:" + smallBeer);
-		setTextView(R.id.small_beer_count, ""+smallBeer);
+//		postToast("small Beer:" + smallBeer);
+		setTextView(R.id.small_beer_count, "" + smallBeer);
 	}
 
 	/**
@@ -95,14 +101,22 @@ public class BeerMe extends Activity {
 	 */
 	public void addLargeBeer(View view) {
 		largeBeer++;
-		postToast("large Beer:" + largeBeer);
-		setTextView(R.id.large_beer_count, ""+largeBeer);
+//		postToast("large Beer:" + largeBeer);
+		setTextView(R.id.large_beer_count, "" + largeBeer);
 	}
 
+	/**
+	 * Method that is called if the Button with the ID 'id/button_reset' is
+	 * clicked.
+	 * 
+	 * @param view
+	 */
 	public void reset(View view) {
 		smallBeer = 0;
 		largeBeer = 0;
-		postToast("reseed all counters");
+//		postToast("reseted all counters");
+		setTextView(R.id.large_beer_count, "" + largeBeer);
+		setTextView(R.id.small_beer_count, "" + smallBeer);
 	}
 
 	/**
@@ -117,9 +131,18 @@ public class BeerMe extends Activity {
 		Toast toast = Toast.makeText(context, s, duration);
 		toast.show();
 	}
-	
-	private void setTextView(int textViewID, String s){
-		TextView tw = (TextView)findViewById(textViewID);
+
+	/**
+	 * Method for setting the text in a TextView wihthe ID textViewID to the
+	 * String s.
+	 * 
+	 * @param textViewID
+	 *            The ID of the TextView that the String s should be set to.
+	 * @param s
+	 *            The String that should be displayed in the TextView.
+	 */
+	private void setTextView(int textViewID, String s) {
+		TextView tw = (TextView) findViewById(textViewID);
 		tw.setText(s);
 	}
 }
